4170e9f67aa56557ae4ef61cbc769fc853d170cd
Antonio Rufino <devs.toni@gmail.com>
2023-09-02 02:51:53 +0200

    refactor: moved files

--- bianxi-api-domain/src/main/java/com/ecommerce/bikes/exception/OrderNotFoundException.java
+++ bianxi-api-domain/src/main/java/com/ecommerce/bikes/exception/OrderNotFoundException.java
@@ -1,7 +1,5 @@
 package com.ecommerce.bikes.exception;
 
-import ch.qos.logback.core.encoder.EchoEncoder;
-
 public class OrderNotFoundException extends RuntimeException {
 
     public OrderNotFoundException(String message) {
